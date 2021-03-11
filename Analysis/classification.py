aa30785cba2d9efe4aa289218544372f2448c06a
joaoflauzino <joaolucas.apoio@gmail.com>
2021-03-10 22:48:02 -0300

    Correção na classe de modelos

--- Analysis/classification.py
+++ Analysis/classification.py
@@ -1,16 +1,7 @@
 # Bibliotecas para modelagem:
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.wrappers.scikit_learn import KerasClassifier
-# from keras.utils import np_utils
-# from sklearn.model_selection import KFold
-# from sklearn.model_selection import cross_val_score
-# from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import accuracy_score
 
 class classification_models(object):
 
@@ -25,9 +16,9 @@ class classification_models(object):
 
     def apply_model(self):
 
-        # for name, model in self.models:
-        #     create_model = model.fit(self.X_train, self.Y_train)
-        #     predictions = create_model.predict(self.X_valid)
-        #     self.results[name] = accuracy_score(self.Y_valid , predictions)
+        for name, model in self.models:
+            create_model = model.fit(self.X_train, self.Y_train)
+            predictions = create_model.predict(self.X_valid)
+            self.results[name] = accuracy_score(self.Y_valid , predictions)
         
-        return self.models
+        return self.results
