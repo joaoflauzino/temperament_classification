1fe76f4846f0ccaed66443980fdd63b9e72f0836
joaoflauzino <joaolucas.apoio@gmail.com>
2021-03-13 19:15:46 -0300

    Novo notebook com testes com o fasttext

--- Analysis/pre_processing.py
+++ Analysis/pre_processing.py
@@ -19,20 +19,6 @@ from spacy.lang.pt.stop_words import STOP_WORDS
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
-# # Bibliotecas para avaliação
-# from sklearn.metrics import confusion_matrix
-# from sklearn.metrics import classification_report 
-# from imblearn.over_sampling import SMOTE
-# from sklearn.metrics import accuracy_score
-# from sklearn.metrics import roc_auc_score
-# from sklearn.metrics import precision_score
-# from sklearn.metrics import recall_score
-# from sklearn.metrics import f1_score
-
-#%matplotlib inline
-#warnings.filterwarnings('ignore')
-
-
 class preprocess_nlp(object):
     
     def __init__(self, texts, stopwords=True, lemma=False, stem=False, wordcloud=True, numeric='tfidf', ngram=1):
