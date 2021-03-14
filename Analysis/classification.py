d14fc58b5a8dfbda38caee68adf2d067e620430d
joaoflauzino <joaolucas.apoio@gmail.com>
2021-03-14 12:08:18 -0300

    Implementando K-fold para garantir a generalização entre instâncias de treino e teste

--- Analysis/classification.py
+++ Analysis/classification.py
@@ -6,36 +6,50 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import RepeatedKFold
 
 class classification_models(object):
 
-    def __init__(self, X_train, Y_train, X_valid, Y_valid, models):
+    def __init__(self, X, Y, models):
 
-        self.X_train = X_train
-        self.Y_train = Y_train
-        self.X_valid = X_valid
-        self.Y_valid = Y_valid
+        self.X = X
+        self.Y = Y
         self.models = models
-        self.results = {}
+        self.X_train = None # k-fold result
+        self.Y_train = None # k-fold result
+        self.X_valid = None # k-fold result
+        self.Y_valid = None # k-fold result
+        self.results = {} # Model results
+
+    def create_struct(self):
+        for name, model in self.models:
+            self.results[name] = {}
+            self.results[name]['acc'] = []
+            self.results[name]['precision'] = []
+            self.results[name]['recall'] = []
+            self.results[name]['f1_score'] = []
+            self.results[name]['auc'] = []
         
     def k_fold(self):
-        pass
-    
+        kf = RepeatedKFold(n_splits=2, n_repeats=30, random_state=20)
+        for linhas_treino, linhas_valid in kf.split(self.X):
+            self.X_train, self.X_valid = self.X.iloc[linhas_treino], self.X.iloc[linhas_valid]
+            self.Y_train, self.Y_valid = self.Y['y_train_res'].iloc[linhas_treino], self.Y['y_train_res'].iloc[linhas_valid]
+            self.apply_model()
+            
     def hist_validation(self):
         pass
 
     def apply_model(self):
-
+    
+        self.create_struct()
         for name, model in self.models:
             create_model = model.fit(self.X_train, self.Y_train)
-            predictions = create_model.predict(self.X_valid)
+            predictions = create_model.predict(self.Y_valid)
+            self.results[name]['acc'].append(accuracy_score(self.Y_valid , predictions))
+            self.results[name]['precision'].append(precision_score(self.Y_valid , predictions))
+            self.results[name]['recall'].append(recall_score(self.Y_valid , predictions))
+            self.results[name]['f1_score'].append(f1_score(self.Y_valid , predictions))
+            self.results[name]['auc'].append(roc_auc_score(self.Y_valid , predictions))
             
-            self.results[name] = {
-                                    "acc": accuracy_score(self.Y_valid , predictions),
-                                    "Precisão": precision_score(self.Y_valid , predictions),
-                                    "Recall": recall_score(self.Y_valid , predictions),
-                                    "F1_score": f1_score(self.Y_valid , predictions, average='macro'),
-                                    "roc_auc_score": roc_auc_score(self.Y_valid , predictions, multi_class='ovr')
-                                  }
-        
         return self.results
